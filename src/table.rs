bc83e5a03ba7d4622e061059283e2ebc06ca0ec7
haruband <haruband@gmail.com>
2024-01-19 11:29:17 +0900

    Add storage and compute execution time logs

--- src/table.rs
+++ src/table.rs
@@ -5,6 +5,7 @@ use arrow::array::RecordBatch;
 use arrow::datatypes::{Schema, SchemaRef};
 use sqlparser::ast::{SetExpr, Statement};
 use std::sync::Arc;
+use std::time::Instant;
 
 pub struct DQTable {
     pub storage: Box<dyn DQStorage>,
@@ -30,9 +31,22 @@ impl DQTable {
         match statement {
             Statement::Query(query) => {
                 if let SetExpr::Select(_) = query.body.as_ref() {
+                    let time0 = Instant::now();
+
                     let files = self.storage.select(statement).await?;
+
+                    log::info!(
+                        "fetched files for {} milliseconds",
+                        time0.elapsed().as_millis()
+                    );
+
                     let schema = self.storage.schema();
                     let batches = self.compute.select(statement, schema, files).await?;
+
+                    log::info!(
+                        "executed queries for {} milliseconds",
+                        time0.elapsed().as_millis()
+                    );
 
                     Ok(batches)
                 } else {
